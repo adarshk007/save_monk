b9e40bd15ddc6dcc6e40c87fcbb1ebed4786a60f
Adarsh kumar <adarsh16007@iiitd.ac.in>
2019-11-12 02:07:32 +0530

    done js

--- src/stores.js
+++ src/stores.js
@@ -18,32 +18,37 @@ api_s.open("GET", "stores.json", true);
 api_s.onload = function() {
 
 	var data = JSON.parse(this.response);
+	var str11=window.location.href;
+	var des=str11.split('?');
+	var des=des[1].split('=');
 	var i=1;
 	for(var j=0;j<data.length;j++)  // for unkown Value then we will use For each Fucntion for Loop
 	{
 	  var beta=data[j];
-    
+		
   	if (api_s.status >= 200 && api_s.status < 400) 
-  		{	    var newNode = document.createElement('li');
-            newNode.className = "table-row";
-            newNode.innerHTML = "<div class='col col-1' data-label='icon'><img src='"+(beta.Logo)+"' class='image_op'/></div>"+
-                          "<div class='col col-2' data-label='Company'>"+(beta.Company)+"</div>"+
-		    	                "<div class='col col-3' data-label='Location'>"+(beta.Location)+"</div>"+
-                          "<div class='col col-4' data-label='Type'>"+(beta.Type)+"</div>"+
-                          "<div style='color:#C33764;' class='col col-5' data-label='Rating'>"+(beta.Rating)+"</div>"+
-                          "<div class='col col-6' data-label='Contact'>"+(beta.Contact)+"</div>" +
-                          "<div class='col col-7' data-label='link'><button class='btn btn-danger' onclick='window.location.href='"+(beta.URL)+"'; return false;'>URL</button></div>"                          
-//                           "<p>"+(beta.Item)+"</p>"+
-//                           "<p style='color:#20002c;'>"+(beta.Company)+"</p>"+
-//                           "<p style='color:#20002c;'>Price "+(beta.Reduced_Price)+"</p>"+
-//                           "<button onclick='tests()'>Add to basket</button>"+
-//                   "</div>"+
-//                   "<div class='product_overlay'>"+
-//                          "<h2>Added to basket</h2>"+
-//                          "<i class='fa fa-check'></i>"+
-//                   "</div>"
-            document.getElementById('stores').appendChild(newNode);    
-
+  		{	
+		if(des[1]==beta.Location || des[1]=="all" || des[1].includes(beta.Type)){
+		    var newNode = document.createElement('li');
+		    newNode.className = "table-row";
+		    newNode.innerHTML = "<div class='col col-1' data-label='icon'><img src='"+(beta.Logo)+"' class='image_op'/></div>"+
+				  "<div class='col col-2' data-label='Company'>"+(beta.Company)+"</div>"+
+						"<div class='col col-3' data-label='Location'>"+(beta.Location)+"</div>"+
+				  "<div class='col col-4' data-label='Type'>"+(beta.Type)+"</div>"+
+				  "<div style='color:#C33764;' class='col col-5' data-label='Rating'>"+(beta.Rating)+"</div>"+
+				  "<div class='col col-6' data-label='Contact'>"+(beta.Contact)+"</div>" +
+				  "<div class='col col-7' data-label='link'><button class='btn btn-danger' onclick='window.location.href='"+(beta.URL)+"'; return false;'>URL</button></div>"                          
+	//                           "<p>"+(beta.Item)+"</p>"+
+	//                           "<p style='color:#20002c;'>"+(beta.Company)+"</p>"+
+	//                           "<p style='color:#20002c;'>Price "+(beta.Reduced_Price)+"</p>"+
+	//                           "<button onclick='tests()'>Add to basket</button>"+
+	//                   "</div>"+
+	//                   "<div class='product_overlay'>"+
+	//                          "<h2>Added to basket</h2>"+
+	//                          "<i class='fa fa-check'></i>"+
+	//                   "</div>"
+		    document.getElementById('stores').appendChild(newNode);    
+		}
  	 }
  	 i=i+1;
  	}
